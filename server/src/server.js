9d2263a2718a640d01f8c05ca1497d1b9930374d
Punit <punitjangra2742@gmail.com>
2026-09-20 15:41:04 +0530

    Wire backend routes and startup configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import app from './app.js'
 import connectDB from './config/db.js'
+import bootstrapSuperAdmin from './services/bootstrapSuperAdmin.js'
 
 const PORT = process.env.PORT || 5000
 
 await connectDB()
+if (process.env.MONGO_URI) await bootstrapSuperAdmin()
 
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`)
