3927b20557e4a84214346e51f1b5b7c52abab70d
Punit <punitjangra2742@gmail.com>
2026-09-20 11:56:04 +0530

    Rebuild reference visual: Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,22 @@
+import { useCallback, useState } from 'react'
 import Navbar from '../components/layout/Navbar'
 import Hero from '../components/sections/Hero'
 import Preloader from '../components/common/Preloader'
-import { useCallback, useState } from 'react'
 import useSmoothScroll from '../hooks/useSmoothScroll'
 
 export default function Home() {
   const [introComplete, setIntroComplete] = useState(false)
   const finishIntro = useCallback(() => setIntroComplete(true), [])
   useSmoothScroll()
+
   return (
     <>
       {!introComplete && <Preloader onComplete={finishIntro} />}
       <div className={introComplete ? 'site site--visible' : 'site'}>
-      <Navbar />
-      <main id="main-content">
-        <Hero />
-      </main>
+        <Navbar />
+        <main id="main-content">
+          <Hero />
+        </main>
       </div>
     </>
   )
