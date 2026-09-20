eb9f29d9d1dbffc27dbd53e7e064d3ae4a8652e9
Punit <punitjangra2742@gmail.com>
2026-09-20 21:11:33 +0530

    Add dedicated Albums route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Portfolio from './pages/Portfolio'
+import Albums from './pages/Albums'
 import WeddingStory from './pages/WeddingStory'
 import Contact from './pages/Contact'
 import Auth from './pages/Auth'
@@ -11,6 +12,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/albums" element={<Albums />} />
       <Route path="/wedding/:slug" element={<WeddingStory />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/login" element={<Auth mode="login" />} />
